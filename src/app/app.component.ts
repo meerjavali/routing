8a076edbffca5be8c7331d64827ef4ac3a8015c2
meerjavali <meerjavali98@gmail.com>
2024-07-06 00:07:12 +0530

    class on 5/7/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'routing';
+
+  ngOnInit(){
+
+  }
+ constructor(private router:Router){} // Dependencies are injected as constructor parameter
+  
+ 
+ OnAboutClick(){/// this method used for programatic navigation
+    //
+    this.router.navigate(['/about']);
+  }
 }
+
+// date 5/7/2024
+// 1. what is Routing 
+// 2. how to register router paths 
+// 3. how to navigate to router paths using router Link 
+// 4.  how to navigate to router paths using programmatic way 
+// 5. routerLinkActive and routerLinkActiveOptions
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { Route, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+
+const routes:Route[]=[
+  {path:'', component:MainComponent},
+  {path:'about' , component:AboutComponent}, // syntax for registering router paths
+  {path:'home', component:HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes) // we are telling our angular application the registred router path information
+
   ],
   providers: [],
   bootstrap: [AppComponent]
