606329817e173ca5b08464f64cbe902737161fdb
meerjavali <meerjavali98@gmail.com>
2024-07-06 23:15:49 +0530

    6/7/2024 routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { AboutsComponent } from './about/child/abouts/abouts.component';
+import { ChildComponent } from './about/child/child.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes:Route[]=[
+  {path:'', component:MainComponent},
+  {path:'about' , component:AboutComponent, children:[
+    {path:':id/:name', component:AboutsComponent},
+    {path:'child', component:ChildComponent}
+  ]}, // syntax for registering router paths
+  {path:'home', component:HomeComponent}
+  // {path:'about/:id/:name', component:AboutsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,36 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { Route, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { AboutsComponent } from './about/child/abouts/abouts.component';
+import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './about/child/child.component';
+//*
+//this code is shifted to app.routing.module.ts
 
-const routes:Route[]=[
-  {path:'', component:MainComponent},
-  {path:'about' , component:AboutComponent}, // syntax for registering router paths
-  {path:'home', component:HomeComponent}
-];
+// const routes:Route[]=[
+//   {path:'', component:MainComponent},
+//   {path:'about' , component:AboutComponent, children:[
+//     {path:':id/:name', component:AboutsComponent},
+//     {path:'child', component:ChildComponent}
+//   ]}, // syntax for registering router paths
+//   {path:'home', component:HomeComponent}
+//   // {path:'about/:id/:name', component:AboutsComponent}
+// ];
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     HomeComponent,
-    MainComponent
+    MainComponent,
+    AboutsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes) // we are telling our angular application the registred router path information
+    FormsModule,
+    RouterModule
+    // RouterModule.forRoot(routes) // we are telling our angular application the registred router path information
 
   ],
   providers: [],
